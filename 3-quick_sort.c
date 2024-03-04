5efa776414e6cf84be363b2eb8a6f393eaaf7755
parksomin72 <fatimazahraealawi@gmail.com>
2024-03-04 06:20:24 -0800

    Quick sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,76 @@
+#include "sort.h"
+
+/**
+ * swap - Swaps two elements in an array
+ * @a: Pointer to the first element
+ * @b: Pointer to the second element
+ */
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
+ * lomuto_partition - Partitions the array using Lomuto partition scheme
+ * @array: The array to be partitioned
+ * @low: The lower index of the partition
+ * @high: The higher index of the partition
+ * @size: The size of the array
+ *
+ * Return: The partition index
+ */
+int lomuto_partition(int *array, int low, int high, size_t size)
+{
+	int pivot;
+	int i;
+	int j;
+
+	pivot = array[high];
+	i = low - 1;
+	for (j = low; j <= high - 1; j++)
+	{
+	    if (array[j] < pivot)
+	    {
+		    i++;
+		    swap(&array[i], &array[j]);
+		    print_array(array, size);
+	    }
+    }
+    swap(&array[i + 1], &array[high]);
+    print_array(array, size);
+    return (i + 1);
+}
+
+/**
+ * quicksort - Recursive function to perform quick sort
+ * @array: The array to be sorted
+ * @low: The lower index of the partition
+ * @high: The higher index of the partition
+ * @size: The size of the array
+ */
+void quicksort(int *array, int low, int high, size_t size)
+{
+	int pi;
+	if (low < high)
+	{
+		pi = lomuto_partition(array, low, high, size);
+
+		quicksort(array, low, pi - 1, size);
+		quicksort(array, pi + 1, high, size);
+	}
+}
+
+/**
+ * quick_sort - Sorts an array of integers in ascending order using Quick Sort algorithm
+ * @array: The array to be sorted
+ * @size: Number of elements in the array
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+
+	quicksort(array, 0, size - 1, size);
+}
--- sort.h
+++ sort.h
@@ -22,6 +22,10 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void print_list(const listint_t *list);
 void selection_sort(int *array, size_t size);
+void swap(int *a, int *b);
+int lomuto_partition(int *array, int low, int high, size_t size);
+void quicksort(int *array, int low, int high, size_t size);
+void quick_sort(int *array, size_t size);
 
 /* Other sorting algorits */
 void print_array(const int *array, size_t size);
